ed9ee78ba4aeb155f8bf02a4443d34d56c0316ff
KHAMED Smaïl <khameds@gmail.com>
2018-01-20 10:38:25 +0100

    [Feat] Organize the code for editor, commentaries.

--- Assets/Scripts/Avatar/PlayerController.cs
+++ Assets/Scripts/Avatar/PlayerController.cs
@@ -5,30 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] private float maxSpeed = 10f;                    // The fastest the player can travel in the x axis.
-    [SerializeField] private float jumpForce = 650f;                  // Amount of force added when the player jumps.
-    [SerializeField] private bool airControl = true;                 // Whether or not a player can steer while jumping;
-    [SerializeField] private LayerMask whatIsGround;                 // A mask determining what is ground to the character
-    [SerializeField] private float wallSlideSpeed = 3f;
-    [SerializeField] private float wallRangeDetection = 0.8f;
+    [SerializeField] private float maxSpeed = 10f;                  // The fastest the player can travel in the x axis.
+    [SerializeField] private float jumpForce = 650f;                // Amount of force added when the player jumps.
+    [SerializeField] private bool airControl = true;                // Whether or not a player can steer while jumping;
+    [SerializeField] private LayerMask whatIsGround;                // A mask determining what is ground to the character
+    [SerializeField] private float wallSlideSpeed = 3f;             // Speed of the player over Y-axis while "wall sliding" 
+    [SerializeField] private float wallRangeDetection = 0.8f;       // Distance from player's collider within colliding objects are detect
+    [SerializeField] private float wallStickTime = 0.25f;           // Wall jump period of time availability
 
-    private Transform groundCheck;    // A position marking where to check if the player is grounded.
-    const float groundedRadius = .1f; // Radius of the overlap circle to determine if grounded
-    public bool grounded;            // Whether or not the player is grounded.
-   // private Transform headCheck;   // A position marking where to check for ceilings
-    const float headRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
-    private Animator animator;            // Reference to the player's animator component.
-    private Rigidbody2D rigidBody;
 
     [HideInInspector] public bool facingRight = true;  // For determining which way the player is currently facing.
-    [HideInInspector]  public Vector2 input;
+    [HideInInspector] public bool grounded;            // Whether or not the player is grounded.
 
-    private bool wallJumpAvailable = true;
-    private bool wantToJump = false;
+    private Transform groundCheck;              // A position marking where to check if the player is grounded.
+    const float groundedRadius = .1f;           // Radius of the overlap circle to determine if grounded
+    
+   // private Transform headCheck;              // A position marking where to check for ceilings
+    private const float headRadius = .01f;      // Radius of the overlap circle to determine if the player can stand up
+    private Animator animator;                  // Reference to the player's Animator component.
+    private Rigidbody2D rigidBody;              // Reference to the player's Rigibody2D component 
+    private bool wantToJump = false;            // Did the player pressed the "Jump" button
+    private float timeToWallUnstick;            // Define if the player is still considered as "wall sliding"
+    private Vector2 input;                      // Store input informations
 
-    private float timeToWallUnstick;
-    private float wallStickTime = 0.25f;
-
+    // At script load
     private void Awake()
     {
         // Setting up references.
@@ -58,8 +58,11 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("vSpeed", rigidBody.velocity.y);
 
 
-        // Wall jump handling
-        Physics2D.queriesStartInColliders = false;
+        /**************************/
+        /*** Wall jump handling ***/
+        /**************************/
+
+        Physics2D.queriesStartInColliders = false; // In order to not collide with our own collider
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, wallRangeDetection);
 
         if (hit.collider != null && rigidBody.velocity.y <= 0 && !grounded)
@@ -72,6 +75,7 @@ public class PlayerController : MonoBehaviour
         else if (hit.collider == null)
             timeToWallUnstick -= Time.deltaTime;
         
+        //If still considered as "wall sliding"
         if (timeToWallUnstick > 0 && !grounded)
         {
             int wallDirection = (this.facingRight) ? 1 : -1;
@@ -82,6 +86,7 @@ public class PlayerController : MonoBehaviour
                 wantToJump = false;
             }
 
+            // If running to the wall, just slide over it
             if (input.x == wallDirection && (input.x != 0))
                 rigidBody.velocity = new Vector2(0, -wallSlideSpeed);
         }
@@ -90,6 +95,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Store input information for FixedUpdate method
         if (!wantToJump)
         {
             // Read the jump input in Update so button presses aren't missed.
