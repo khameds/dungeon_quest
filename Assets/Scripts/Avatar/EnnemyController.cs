508526f0129ef90656a113f8bbe71955fbf4da72
KHAMED Smaïl <khameds@gmail.com>
2018-01-20 16:18:46 +0100

    [Feat] Base IA pathing requiring proper terain definition.

--- Assets/Scripts/Avatar/EnnemyController.cs
+++ Assets/Scripts/Avatar/EnnemyController.cs
@@ -1,123 +1,109 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Seeker))]
 public class EnnemyController : MonoBehaviour
 {
+    // TODO : get this from players list
+    public Transform target;        // Chased target position
+    public float updateRate = 2f;   // Update rate of A* path
+    private Seeker seeker;          // Pathing script
+    private Rigidbody2D rb;         // Referance to the Rigidbody2D component of the IA
+    public Path path;               //The calculated path  
+    public float speed = 300f;      //The AI's speed per second
+    public ForceMode2D fMode;
 
-    [SerializeField] private float maxSpeed = 10f;                    // The fastest the player can travel in the x axis.
-    [SerializeField] private float jumpForce = 400f;                  // Amount of force added when the player jumps.
-    [SerializeField] private bool airControl = true;                 // Whether or not a player can steer while jumping;
-    [SerializeField] private LayerMask whatIsGround;                 // A mask determining what is ground to the character
-    [SerializeField] private float wallSlideSpeed = 3f;
-    [SerializeField] private float wallRangeDetection = 0.8f;
+    [HideInInspector] public bool pathIsEnded = false;
 
-    private Transform groundCheck;    // A position marking where to check if the player is grounded.
-    const float groundedRadius = .1f; // Radius of the overlap circle to determine if grounded
-    public bool grounded;            // Whether or not the player is grounded.
-                                     // private Transform headCheck;   // A position marking where to check for ceilings
-    const float headRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
-    private Animator animator;            // Reference to the player's animator component.
-    private Rigidbody2D rigidBody;
-    public bool facingRight = true;  // For determining which way the player is currently facing.
-    private bool jumping = false;
-    Vector2 input;
-    private bool wallJumpAvailable = true;
+    // The max distance from the AI to a waypoint for it to continue to the next waypoint
+    public float nextWaypointDistance = 3;
 
-    private void Awake()
+    // The waypoint we are currently moving towards
+    private int currentWaypoint = 0;
+
+    void Start()
     {
-        // Setting up references.
-        groundCheck = transform.Find("GroundCheck");
-        //headCheck = transform.Find("HeadCheck");
-        animator = GetComponent<Animator>();
-        rigidBody = GetComponent<Rigidbody2D>();
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
 
-        input = new Vector2();
+        if (target == null)
+        {
+            // TODO get this from players list
+            Debug.LogError("Can't find any player alive, GAMEOVER.");
+            return;
+        }
+
+        // Start a new path to the target position, return the result to the OnPathComplete method
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        StartCoroutine(UpdatePath());
     }
 
-
-    private void FixedUpdate()
+    IEnumerator UpdatePath()
     {
-        grounded = false;
-        bool wallSliding = false;
-
-        // The enemy is grounded if a circlecast to the groundcheck position hits anything designated as ground
-        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
-        for (int i = 0; i < colliders.Length; i++)
+        if (target == null) // TODO test if player alive
         {
-            if (colliders[i].gameObject != gameObject)
-                grounded = true;
-        }
-        animator.SetBool("Ground", grounded);
-
-        // Set the vertical animation
-        animator.SetFloat("vSpeed", rigidBody.velocity.y);
-
-
-        // Wall jump handling
-        Physics2D.queriesStartInColliders = false;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, wallRangeDetection);
-
-        if (hit.collider != null && rigidBody.velocity.y <= 0 && !grounded)
-        {
-            wallSliding = true;
-            if (rigidBody.velocity.y < -wallSlideSpeed)
-                rigidBody.velocity = new Vector2(rigidBody.velocity.x, -wallSlideSpeed);
+            // TODO get this from players list
+            Debug.LogError("Can't find any player alive, GAMEOVER.");
+            yield break;
         }
 
-        if (wallSliding && !grounded)
-        {
+        // Start a new path to the target position, return the result to the OnPathComplete method
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
 
-            int wallDirection = (this.facingRight) ? 1 : -1;
-
-            if (Input.GetButtonDown("Jump") && (input.x != wallDirection) && wallJumpAvailable)
-            {
-                rigidBody.AddForce(new Vector2(0f, jumpForce));
-                wallJumpAvailable = false;
-            }
-
-            if (input.x == wallDirection && (input.x != 0))
-                rigidBody.velocity = new Vector2(0, -wallSlideSpeed);
-        }
-
-        if (hit.collider == null)
-            wallJumpAvailable = true;
-
+        yield return new WaitForSeconds(1f / updateRate);
+        StartCoroutine(UpdatePath());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPathComplete(Path p)
     {
-
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+        else Debug.Log("We got a path with an error.");
+        
     }
 
-    public void Move(float move, bool jump)
+    void FixedUpdate()
     {
-        if (grounded || airControl)
+        if (target == null)
         {
-            //TODO
-
+            //TODO: Insert a player search here.
+            return;
         }
 
-        // If the player should jump...
-        if (grounded && jump && animator.GetBool("Ground"))
+        //TODO: Always look at player?
+
+        if (path == null)
+            return;
+
+        if (currentWaypoint >= path.vectorPath.Count)
         {
-            // Add a vertical force to the player.
-            grounded = false;
-            animator.SetBool("Ground", false);
-            rigidBody.AddForce(new Vector2(0f, jumpForce));
+            if (pathIsEnded)
+                return;
+
+            Debug.Log("End of path reached.");
+            pathIsEnded = true;
+            return;
         }
-    }
+        pathIsEnded = false;
 
-    public void Flip()
-    {
-        // Switch the way the player is labelled as facing.
-        facingRight = !facingRight;
+        //Direction to the next waypoint
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        dir *= speed * Time.fixedDeltaTime;
 
-        // Multiply the player's x local scale by -1.
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        //Move the AI
+        rb.AddForce(dir, fMode);
+
+        float dist = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
+        if (dist < nextWaypointDistance)
+        {
+            currentWaypoint++;
+            return;
+        }
     }
 }
