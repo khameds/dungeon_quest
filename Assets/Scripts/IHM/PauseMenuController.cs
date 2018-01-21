d79e65a8a255ef613e8394eb2e163e10329c229e
dimitribruyere <dimitri.bruyere@outlook.fr>
2018-01-21 16:52:35 +0100

    We can now pause the game and resume

---
+++ Assets/Scripts/IHM/PauseMenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour {
+
+    public GameObject menuObject;
+    private bool isActive = false; 
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (isActive == true)
+        {
+            menuObject.SetActive(true);
+            
+            //TODO 
+            //We must see the cursor
+            //Cursor.visible = true;
+            
+            //We stop the game by stopping the time
+            Time.timeScale = 0;
+        }
+        else
+        {
+            menuObject.SetActive(false);
+
+            //TODO
+            //We must see the cursor
+            //Cursor.visible = false;
+
+            //We resume the game
+            Time.timeScale = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isActive = !isActive;
+        }
+	}
+
+    public void Resume()
+    {
+        isActive = !isActive;
+    }
+}
