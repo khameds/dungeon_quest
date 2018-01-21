1177f844d11a28e6ade41f9797ccba361ff2421e
dimitribruyere <dimitri.bruyere@outlook.fr>
2018-01-21 12:06:01 +0100

    We can reach the createGameSolo interface via the mainMenu

---
+++ Assets/Scripts/IHM/mainMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenuController : MonoBehaviour {
+
+    // Permit to acceed to the scene in paramater
+	public void NewScene(string theScene)
+    {
+        SceneManager.LoadScene(theScene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
