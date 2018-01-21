f7eda80ab94646c89f9673ccd0f0d1be435d70cd
dimitribruyere <dimitri.bruyere@outlook.fr>
2018-01-21 14:24:10 +0100

    Buttons for return to the mainMenu

--- Assets/Scripts/IHM/MenuController.cs
+++ Assets/Scripts/IHM/MenuController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class mainMenuController : MonoBehaviour {
+public class MenuController : MonoBehaviour {
 
     // Permit to acceed to the scene in paramater
 	public void NewScene(string theScene)
     {
-        SceneManager.LoadScene(theScene);
+        SceneManager.LoadScene(theScene, LoadSceneMode.Single);
+
     }
 
     public void ExitGame()
