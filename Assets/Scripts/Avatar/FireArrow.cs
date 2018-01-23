d2ec7e8b19f626e73647a31e065edf45cdc360f0
Chris <christopher@jeamme.fr>
2018-01-23 03:19:20 +0100

    add script for Weapon sight

---
+++ Assets/Scripts/Avatar/FireArrow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireArrow : MonoBehaviour
+{
+    public Rigidbody2D sight;
+    public Rigidbody2D character;
+    
+    public float maxDragDistance = 2f; //We can set it by the type of the selected weapon
+
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Vector3.Distance(mousePos, character.position) > maxDragDistance)
+            sight.position = character.position + (mousePos - character.position).normalized * maxDragDistance;
+        else
+            sight.position = mousePos;
+    }
+
+    void OnMouseDown()
+    {
+        //Switch on weapon selected and FIREEEEEE if enough ammo
+
+    }
+
+    void OnMouseUp()
+    {
+        //For weapons that require letting the key press
+    }
+}
