2e04a7cd2e2a0f7dace41291dbfef04484dd1721
Chris <christopher@jeamme.fr>
2018-01-25 06:24:54 +0100

    add LevelParam script file

---
+++ Assets/Scripts/LevelParam.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Static class to give parameters to a level from the menu
+public class LevelParam
+{
+    //Object where we stock string
+    private static Dictionary<string,string> _params = new Dictionary<string, string>();
+
+    // Get a string parameter
+    public static string Get (string key)
+    {
+        if (_params.ContainsKey(key))
+            return _params[key];
+        return null;
+	}
+
+    // Set a string parameter
+    public static void Set (string key, string value)
+    {
+        if (_params.ContainsKey(key))
+            _params[key] = value;
+        else
+            _params.Add(key, value);
+    }
+
+    // Clear all parameters
+    public static void Clear ()
+    {
+        _params.Clear();
+    }
+}
