87ece2c29ce70204cc3dfd35d5a6254ed0be18e6
ChrisJeamme <christopher@jeamme.fr>
2018-01-29 13:55:51 +0100

    feat: script to display text alerts on the scene

---
+++ Assets/Scripts/Gameplay/DisplayAlert.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayAlert : MonoBehaviour
+{
+    public static DisplayAlert instance;
+    public static Text manche;
+
+    public void Start()
+    {
+        //Binding the text in the scene
+        manche = GameObject.Find("TextAlert").GetComponent<Text>();
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public static void Print(string text)
+    {   
+        manche.text = text;
+        instance.StartCoroutine("LimitedDisplay");
+    }
+        
+    IEnumerator LimitedDisplay()
+    {
+        yield return new WaitForSeconds(2);
+
+        manche.text = "";
+    }
+}
