a3171e2523d9a2b66e6c7817fffb9b95c9bedc2d
ChrisJeamme <christopher@jeamme.fr>
2018-02-14 11:06:48 +0100

    feat: add script to manage gameover menu

---
+++ Assets/Scripts/IHM/GameOverMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenuController : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("sandbox", LoadSceneMode.Single);
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene("mainMenu", LoadSceneMode.Single);
+    }
+}
