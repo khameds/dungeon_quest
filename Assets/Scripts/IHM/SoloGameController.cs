1675018d2c2b1c4bee7f7f43ecb7d38d688823b0
dimitribruyere <dimitri.bruyere@outlook.fr>
2018-02-20 17:18:55 +0100

    feat: We can chose the avatar, the level and the difficulty for a solo game

---
+++ Assets/Scripts/IHM/SoloGameController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoloGameController : MonoBehaviour
+{
+    public GameObject avatarList;
+    public GameObject levelList;
+    private GameObject[] avatars;
+    private GameObject[] levels;
+    private int indexAvatar;
+    private int indexLevel;
+    public Toggle difficultyNormal;
+
+    private void Start()
+    {
+        avatars = new GameObject[avatarList.transform.childCount];
+        levels = new GameObject[levelList.transform.childCount];
+
+        //Fill the array
+        for(int i=0; i< avatarList.transform.childCount; i++)
+        {
+            avatars[i] = avatarList.transform.GetChild(i).gameObject;
+        }
+        for (int i = 0; i < levelList.transform.childCount; i++)
+        {
+            levels[i] = levelList.transform.GetChild(i).gameObject;
+        }
+
+        //Toggle off the renderer
+        foreach (GameObject go in avatars)
+        {
+            go.SetActive(false);
+        }
+        foreach (GameObject go in levels)
+        {
+            go.SetActive(false);
+        }
+        
+        //Toggle the first index
+        if (avatars[0])
+            avatars[0].SetActive(true);
+        if (levels[0])
+            levels[0].SetActive(true);
+    }
+
+    public void ToggleAvatar(int direction)
+    {
+        //Toggle off the current avatar
+        avatars[indexAvatar].SetActive(false);
+
+        indexAvatar += direction;
+        if (indexAvatar < 0)
+            indexAvatar = avatars.Length - 1;
+        if (indexAvatar >= avatars.Length)
+            indexAvatar = 0;
+
+        //Toggle on the new avatar
+        avatars[indexAvatar].SetActive(true);
+    }
+
+    public void ToggleLevel(int direction)
+    {
+        //Toggle off the current avatar
+        levels[indexLevel].SetActive(false);
+
+        indexLevel += direction;
+        if (indexLevel < 0)
+            indexLevel = levels.Length - 1;
+        if (indexLevel >= levels.Length)
+            indexLevel = 0;
+
+        //Toggle on the new avatar
+        levels[indexLevel].SetActive(true);
+    }
+
+    public void createGame()
+    {
+        LevelParam.Set("Avatar1", indexAvatar.ToString());
+        if (difficultyNormal.isOn)
+            LevelParam.Set("Difficulty", "normal");
+        else
+            LevelParam.Set("Difficulty", "hard");
+
+        LevelParam.Set("Level", indexLevel.ToString());
+        //Load Scene
+
+
+    }
+}
