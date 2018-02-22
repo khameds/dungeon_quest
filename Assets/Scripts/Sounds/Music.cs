8d0cd954831ccb964cc688344e347d6530a44790
dimitribruyere <dimitri.bruyere@outlook.fr>
2018-02-22 15:17:25 +0100

    feat: Add music in the game

---
+++ Assets/Scripts/Sounds/Music.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+    Object[] myMusic;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        myMusic = Resources.LoadAll("Music", typeof(AudioClip));
+        GetComponent<AudioSource>().clip = myMusic[0] as AudioClip;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
